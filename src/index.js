c0f4622a1fc32f167078cf6ec2582ecd8ab26bd6
Saurabh Puri <purisaurabh2885@gmail.com>
2024-05-24 16:59:30 +0530

    wrap application with redux store

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+import store from "./api/store/store";
 import App from "./App";
 
 import "./index.css";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID}>
-      <App />
-    </GoogleOAuthProvider>
+    <Provider store={store}>
+      <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID}>
+        <App />
+      </GoogleOAuthProvider>
+    </Provider>
   </React.StrictMode>
 );
