479dd8df1e4fa1d0753ab6d5675970bd495350f7
Saurabh Puri <purisaurabh2885@gmail.com>
2024-05-24 17:00:11 +0530

    configure the redux store

---
+++ src/api/store/store.jsx
@@ -0,0 +1,26 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
+import { loginApi } from "../loginApi";
+import { profileApi } from "../profileApi";
+import authReducer from "./authSlice";
+
+const token = window.localStorage.getItem("token");
+const preloadedState = {
+  auth: {
+    token: token ? token : null,
+  },
+};
+const rootReducer = combineReducers({
+  [loginApi.reducerPath]: loginApi.reducer,
+  [profileApi.reducerPath]: profileApi.reducer,
+  auth: authReducer,
+});
+
+const store = configureStore({
+  reducer: rootReducer,
+  preloadedState,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(loginApi.middleware, profileApi.middleware),
+});
+
+export default store;
