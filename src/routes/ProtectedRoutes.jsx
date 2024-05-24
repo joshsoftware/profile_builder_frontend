94445d1e882904fc609a4f4b19b0e1d47cc586c7
Saurabh Puri <purisaurabh2885@gmail.com>
2024-05-24 16:55:24 +0530

    get token from store for request interceptors

--- src/routes/ProtectedRoutes.jsx
+++ src/routes/ProtectedRoutes.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import PropTypes from "prop-types";
 
-// TODO :store the token in the redux store
+import PropTypes from "prop-types";
 
 const ProtectedRoutes = (props) => {
   const navigate = useNavigate();
-  const token = window.localStorage.getItem("token");
+  const token = useSelector((state) => state.auth.token);
   const { Component } = props;
   useEffect(() => {
     if (!token) {
