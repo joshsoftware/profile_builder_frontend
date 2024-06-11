e0affcbe26bb3e62af5d2908333890fc18c9d66a
Saurabh Puri <purisaurabh2885@gmail.com>
2024-06-11 13:25:56 +0530

    update the store.jsx file

--- src/api/store/store.jsx
+++ src/api/store/store.jsx
@@ -6,20 +6,20 @@ import authReducer from "./authSlice";
 const token = window.localStorage.getItem("token");
 const preloadedState = {
   auth: {
-    token: token ? token : null,
-  },
+    token: token ? token : null
+  }
 };
 const rootReducer = combineReducers({
   [loginApi.reducerPath]: loginApi.reducer,
   [profileApi.reducerPath]: profileApi.reducer,
-  auth: authReducer,
+  auth: authReducer
 });
 
 const store = configureStore({
   reducer: rootReducer,
   preloadedState,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(loginApi.middleware, profileApi.middleware),
+    getDefaultMiddleware().concat(loginApi.middleware, profileApi.middleware)
 });
 
 export default store;
