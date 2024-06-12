daefe088841940d306a06f400cc13d202e119fd7
Saurabh Puri <purisaurabh2885@gmail.com>
2024-06-12 16:48:38 +0530

    convert the code into antd component

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,18 +1,25 @@
 import React from "react";
+import { Button, Col, Image, Row, Typography } from "antd";
 import resumeSvg from "../../assets/banner.svg";
 import styles from "./Dashboard.module.css";
 
+const { Title, Text } = Typography;
+
 const Dashboard = () => {
   return (
-    <div className={styles.container}>
-      <img src={resumeSvg} alt="Resume" />
-      <p className={styles.heading}>
-        A <span>Profile</span> that Stands Out! Make Your Own Profile and
-        <span>Download as PDF.</span>
-      </p>
-
-      <button className={styles.createBtn}>Create My Resume</button>
-    </div>
+    <Row className={styles.container} align="middle" justify="center">
+      <Col>
+        <Image src={resumeSvg} alt="Resume" preview={false} />
+        <Title level={2} className={styles.heading}>
+          A <Text strong>Profile</Text> that Stands Out! Make Your Own Profile
+          and
+          <Text strong> Download as PDF.</Text>
+        </Title>
+        <Button type="primary" className={styles.createBtn}>
+          Create My Resume
+        </Button>
+      </Col>
+    </Row>
   );
 };
 
