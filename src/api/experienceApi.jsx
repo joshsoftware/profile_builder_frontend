b51431b0bbd2668a012e7039286167c350e9cc58
Saurabh Puri <purisaurabh2885@gmail.com>
2024-06-26 11:42:26 +0530

    add functionality to api call for create experience

---
+++ src/api/experienceApi.jsx
@@ -0,0 +1,27 @@
+import { createApi } from "@reduxjs/toolkit/query/react";
+import {
+  CREATE_EXPERIENCE_ENDPOINTS,
+  EXPERIENCE_REDUCER_PATH,
+  EXPERIENCE_TAG_TYPES,
+  HTTP_METHODS
+} from "../Constants";
+import axiosBaseQuery from "./axiosBaseQuery/service";
+
+export const experienceApi = createApi({
+  reducerPath: EXPERIENCE_REDUCER_PATH,
+  baseQuery: axiosBaseQuery(),
+  tagTypes: EXPERIENCE_TAG_TYPES,
+  endpoints: (builder) => ({
+    createExperience: builder.mutation({
+      query: ({ profile_id, values }) => ({
+        url: CREATE_EXPERIENCE_ENDPOINTS.replace(":id", profile_id),
+        method: HTTP_METHODS.POST,
+        data: { experiences: values }
+      }),
+      invalidatesTags: EXPERIENCE_TAG_TYPES,
+      transformResponse: (response) => response.data
+    })
+  })
+});
+
+export const { useCreateExperienceMutation } = experienceApi;
