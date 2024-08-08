6cd21ad3a32a3bc04e06ca1b07a88f61d3cea159
Abhishek-Jain-1925 <jainabhishek1925@gmail.com>
2024-08-08 14:12:11 +0530

    [feat10]:project duration validation

--- src/components/Builder/Project/index.jsx
+++ src/components/Builder/Project/index.jsx
@@ -8,7 +8,6 @@ import {
   DatePicker,
   Form,
   Input,
-  InputNumber,
   Row,
   Select,
   Space,
@@ -337,17 +336,8 @@ const Project = ({ projectData }) => {
                       <Form.Item
                         name={[`project_${index}`, "duration"]}
                         label="Project Duration (in years)"
-                        rules={[
-                          {
-                            type: "number",
-                            min: 0,
-                            max: 50,
-                            message:
-                              "Project duration must be between 0 and 50 years",
-                          },
-                        ]}
                       >
-                        <InputNumber placeholder="Eg. 1, 2.5" />
+                        <Input type="number" placeholder="Eg. 1, 2.5" />
                       </Form.Item>
                     </Col>
                   </Row>
